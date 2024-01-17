ed76a792239dd60feb7bb4492c6299008e66f7f0
Polzocr <micro_cpt_62@outlook.fr>
2024-01-17 18:29:49 +0100

    commencement ajout data

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -1,12 +1,15 @@
 import './index.css'
 import photo from '../../assets/photo.jpg'
+import asideDatas from '../../data/aside-data'
 
 export default function Aside(){
+    
     return (
         <>
             <div className='container-photo'>
                 <img src={photo} alt="photo-profil" />
             </div>
+            
         </>
     )
 }
--- src/data/aside-data.js
+++ src/data/aside-data.js
@@ -1,3 +1,25 @@
-const asideDatas = [
-    
+export default [
+    {
+        "Contact" : [
+            {"Téléphone" : "0762749208"},
+            {"Email" : "pw.prof@outlook.fr"},
+            {"Adresse" : "Paris, France"},
+            {"Linkedin" : "https://www.linkedin.com/in/paul-w11"},
+            {"GitHub" : "https://github.com/polzocr"},
+
+        ]
+    },
+    {
+        "Formation" : ""
+    },
+    {
+        "Compétences" : ""
+    },
+    {
+        "Langues" : ""
+    },
+    {
+        "Loisirs" : ""
+    }
 ]
+    
