0c5cbd3d95e9890f13cfe2f96fd8d09932c00d62
Polzocr <micro_cpt_62@outlook.fr>
2024-01-17 18:12:08 +0100

    ajout photo

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -1,9 +1,12 @@
 import './index.css'
+import photo from '../../assets/photo.jpg'
 
 export default function Aside(){
     return (
         <>
-        
+            <div className='container-photo'>
+                <img src={photo} alt="photo-profil" />
+            </div>
         </>
     )
 }
